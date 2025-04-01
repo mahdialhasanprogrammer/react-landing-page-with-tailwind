e92bdbaca2dab88c4c27fe277cb156a8e1be4278
mahdigithubr <mahdialhasan5020@gmail.com>
2025-04-01 21:54:54 +0600

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ function App() {
   return (
     <>
       <header className="container max-auto">
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>Location</li>
-            <li>About</li>
-            <li>Contact</li>
+        <nav className="flex justify-between">
+          <div>
+            <img url="./assets/images/brand_logo.png" alt="brand logo" />
+          </div>
+          <ul className="flex gap-5">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Location</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
           </ul>
+          <div>
+            <button type="button" className="bg-primary text-white px-5 py-2 ">
+              Login
+            </button>
+          </div>
         </nav>
       </header>
     </>
