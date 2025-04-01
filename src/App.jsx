493cc023d3e95add231155be72bca8ecba8f8ca4
mahdigithubr <mahdialhasan5020@gmail.com>
2025-04-01 21:30:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <header className="container max-auto">
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Location</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default App;
